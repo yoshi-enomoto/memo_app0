abc4929a0dd3cba182a6b25d3387bf0a76084abf
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-03 20:39:55 +0900

    一連の流れを作成

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -6,12 +6,19 @@ class TodosController < ApplicationController
 
   def create
     @todo = Todo.new(todo_params)
+    if @todo.save
+      redirect_to root_path, notice: "保存が完了しました"
+    else
+      flash.now[:alert] = "Todoを入力してください"
+      @todos = Todo.all
+      render :index
+    end
   end
 
   private
   def todo_params
+    # 『form_for』で送っている為、『require(:todo)』が必要
     params.require(:todo).permit(:content)
-    # require(:todo).permit(:content)：検証用に記載残し
   end
 
 end
