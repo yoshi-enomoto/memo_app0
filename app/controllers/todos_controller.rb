62fe3dbfb323fb856735a0ec67df967180b8c9f2
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-03 19:53:18 +0900

    コントローラの仮設定

--- app/controllers/todos_controller.rb
+++ app/controllers/todos_controller.rb
@@ -1,7 +1,17 @@
 class TodosController < ApplicationController
   def index
+    @todos = Todo.all
+    @todo = Todo.new
   end
 
   def create
+    @todo = Todo.new(todo_params)
   end
+
+  private
+  def todo_params
+    params.require(:todo).permit(:content)
+    # require(:todo).permit(:content)：検証用に記載残し
+  end
+
 end
