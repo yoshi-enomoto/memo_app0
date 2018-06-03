13858a249a3ba7267c4ee0e8fca31e0ac98ce4bc
榎本 芳昭 <yoshi.another.ac@gmail.com>
2018-06-04 01:44:54 +0900

    ajax用必要情報の記載

--- app/assets/javascripts/todo.js
+++ app/assets/javascripts/todo.js
@@ -1,7 +1,22 @@
 $(function(){
   $("form").on("submit", function(e) {
     e.preventDefault();
-    var todo = $(".form__text-field").val()
-    console.log(todo);
+    var todo = $(".form__text-field").val();
+    $.ajax({
+      // http通信の種類記述
+      type: "post",
+      // リクエスト送信先のurl
+      url: "/todos.json",
+      // サーバーに送信する値を記述
+      // 『params.require{:todo).permit(:content)』と重ね合わせられる？
+      data: {
+        todo: {
+          content: todo
+        }
+      },
+      // サーバーから返されるデータの型を指定
+      dataType: "json"
+    })
+
   });
 });
